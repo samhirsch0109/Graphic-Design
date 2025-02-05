c63eafa2e5f260bb76cded70f54e710fd4dae538
lizreznik <525ereznik@frhsd.com>
2025-02-05 10:11:38 -0500

    added pictures to class page

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
 $(document).ready(function () {
-    $('.teacher-name').on('click', function () {
-        $(this).next('.teacher-details').slideToggle(); // Toggle the next details
+    $('.question').click(function () {
+        $('.hiddenContent').slideToggle();
+    });
+
+
+    $(document).ready(function () {
+        $('.toggle-button').on('click', function () {
+            $('.information').toggle();
+        });
+    });
+
+    $('.project-image').on('mouseenter', function (event) {
+        $(event.currentTarget).next('.project-description').fadeIn(800);
+    });
+
+    $('.project-image').on('mouseleave', function (event) {
+        $(event.currentTarget).next('.project-description').fadeOut(500);
     });
 })
-
-
-$(document).ready(function () {
-    $('.toggle-button').on('click', function () {
-        $('.information').toggle();
-    });
-});
-
-$('.project-image').on('mouseenter', function (event) {
-    $(event.currentTarget).next('.project-description').fadeIn(800);
-});
-
-$('.project-image').on('mouseleave', function (event) {
-    $(event.currentTarget).next('.project-description').fadeOut(500);
-});
